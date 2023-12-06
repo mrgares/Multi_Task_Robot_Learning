3b6c8d235e3c7e61e28bc7685bf5965041226f34
Marcelo <chelo_ge@hotmail.com>
2023-12-06 00:08:00 +0000

    feat: adding a helper function to plot end effector trajectory positions

--- src/helper.py
+++ src/helper.py
@@ -2,7 +2,8 @@ import os
 import robomimic.utils.file_utils as FileUtils
 from robomimic import DATASET_REGISTRY
 import numpy as np
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def download_dataset(task: str, dataset_type: str, hdf5_type: str, download_dir: str):
     if not os.path.exists(download_dir):
@@ -54,3 +55,30 @@ def extract_trajectory_i(hdf5_object, trajectory_idx):
     goal_state = np.concatenate([goal_end_effector_pos, goal_end_effector_rot, goal_gripper_joint_pos, goal_object], axis=-1)
     print(f"    - Goal State: {goal_state.shape}")
     return states, goal_state, actions
+
+def plot_end_effector_trajectory(end_eff_pos, end_eff_goal_pos):
+    """
+    Plots the end effector trajectory for a given trajectory.
+
+    Args:
+        end_eff_pos (np.ndarray): array of end_eff_pos
+        end_eff_goal_pos (np.ndarray): goal end_eff_goal_pos
+        
+    """
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Plotting all states in blue
+    ax.scatter(end_eff_pos[:, 0], end_eff_pos[:, 1], end_eff_pos[:, 2], marker='o', s=10, color='blue', label='Intermediate Poses')
+    # Plotting the first state in red
+    ax.scatter(end_eff_pos[0, 0], end_eff_pos[0, 1], end_eff_pos[0, 2], marker='o', s=30, color='red', label='Start Pose')
+    # Plotting the last state in green
+    ax.scatter(end_eff_goal_pos[0], end_eff_goal_pos[1], end_eff_goal_pos[2], marker='o', s=30, color='green', label='Goal Pose')
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    # Adding a legend
+    ax.legend()
+    plt.show()
